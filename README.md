01d1e4484650244971cbf9054c0967b5dc634905
Julian Johannesen <julian.johannesen@gmail.com>
2018-03-15 19:21:50 -0400

    Minor adjustments to format code

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Pixel Art Maker Project
 
-The Pixel Art Maker was the final project in Udacity's Grow with Google Front-End Developement for Beginners course. Each student in the course was provided with some basic html and css and asked to code an application that will allow the user to draw in a color of their choosing on a grid created by an html table. 
+The Pixel Art Maker was the final project in Udacity's Grow with Google Front-End Developement for Beginners course. Each student in the course was provided with some basic html and css and asked to code an application that will allow the user to draw in a color of their choosing on a grid created by an html table.
 
+Randome notes:
+-- Try using table.insertRow and row.insertCell in the vanilla js version
+-- User DOMContentLoaded to ensure DOM loads first
+
+```javascript
+<script>
+  document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+  });
+</script>
+```
--- designs.js
+++ designs.js
@@ -1,28 +1,28 @@
 // DOM objects
 // Size picker form
-const sizePickerForm = $('#sizePicker');
+const sizePickerForm = $("#sizePicker");
 // Height and width inputs
-const inputHeight = $('#inputHeight')
-const inputWidth = $('#inputWidth');
+const inputHeight = $("#inputHeight");
+const inputWidth = $("#inputWidth");
 // Size picker submit button
 const sizePickerSubmit = $('#sizePicker input[type="submit"]');
 // Canvas, aka the html table grid
-const pixelCanvas = $('#pixelCanvas');
+const pixelCanvas = $("#pixelCanvas");
 // Color picker input
-const colorPicker = $('#colorPicker');
+const colorPicker = $("#colorPicker");
 // Select color input
-let color = '#000000';
+let color = "#000000";
 // Declare and assign table array & row & col elements
 let table = [];
-const row = '\<tr\>\<\/tr\>';
-const col = '\<td\>\<\/td\>';
+const row = "<tr></tr>";
+const col = "<td></td>";
 
-function makeTable(){
-    // Get height and width
-    const height = inputHeight.val();
-    const width = inputWidth.val();
+function makeTable() {
+  // Get height and width
+  const height = inputHeight.val();
+  const width = inputWidth.val();
 
-    /*
+  /*
     [
         { 'row0': ['<tr></tr>', '<td></td', '<td></td>', ...] },  
         { 'row1': ['<tr></tr>', '<td></td', '<td></td>', ...] },
@@ -30,74 +30,75 @@ function makeTable(){
     ]
     */
 
-    // Create table
-    for(let i = 0; i < height; i++){
-        let rowName = 'row' + i;
-        table[i] = {[rowName]: [row]};
-        for(let j = 0; j < width; j++){
-            table[i][rowName].push(col);
-        }
+  // Create table
+  for (let i = 0; i < height; i++) {
+    let rowName = "row" + i;
+    table[i] = {
+      [rowName]: [row]
+    };
+    for (let j = 0; j < width; j++) {
+      table[i][rowName].push(col);
     }
-    return table;
+  }
+  return table;
 }
 
-function insertTableHtml(){
+function insertTableHtml() {
+  // 2 problems
+  // First, I need to reset the table object and DOM when someone hits submit
+  // Second, the last TR is not getting its td's
 
-    // 2 problems 
-    // First, I need to reset the table object and DOM when someone hits submit
-    // Second, the last TR is not getting its td's
+  // Read off the table object to create the table html
 
-    // Read off the table object to create the table html
+  // For each row object in the table array, add a tr to the DOM
+  for (let k = 0; k < table.length; k++) {
+    // Identify the current table row
+    let currentRowArray = table[k]["row" + k];
+    // Identify the current row's tr element
+    let theTR = currentRowArray[0];
+    // Add the tr element to the DOM
+    pixelCanvas.append(theTR);
 
-    // For each row object in the table array, add a tr to the DOM
-    for (let k = 0; k < table.length; k++){
-        // Identify the current table row
-        let currentRowArray = table[k]['row' + k];
-        // Identify the current row's tr element
-        let theTR = currentRowArray[0];
-            // Add the tr element to the DOM
-            pixelCanvas.append(theTR);
-
-        // For each tr added, now add the correct number of td's
-        // Start "l" at 1 to skip over the tr element, 
-        // which is the 1st member of the row array
-        for (let l = 1; l <= currentRowArray.length; l++){
-            // Identify the tr element that was just added to the DOM
-            let theCurrentTr = `#pixelCanvas tr:eq(${k})`;
-            // Add the table data element to the tr element
-            $(theCurrentTr).append(currentRowArray[l]);  
-        }
+    // For each tr added, now add the correct number of td's
+    // Start "l" at 1 to skip over the tr element,
+    // which is the 1st member of the row array
+    for (let l = 1; l <= currentRowArray.length; l++) {
+      // Identify the tr element that was just added to the DOM
+      let theCurrentTr = `#pixelCanvas tr:eq(${k})`;
+      // Add the table data element to the tr element
+      $(theCurrentTr).append(currentRowArray[l]);
     }
+  }
 }
 
 // Clicking on the submit button will call makeGrid. makeGrid calls makeTable
 // and instertTableHtml to create and insert the html table grid.
 function makeGrid(event) {
-    // Prevent form submit
-    event.preventDefault();
-    // Populate the table object
-    makeTable();
-    // Create and insert the table html
-    insertTableHtml();
+  // Prevent form submit
+  event.preventDefault();
+  // Populate the table object
+  makeTable();
+  // Create and insert the table html
+  insertTableHtml();
 }
 
 // Clicking "okay" in the color picker will call watchColorPicker. watchColorPicker
 // sets a color
-function watchColorPicker(event){
-    return color = event.target.value;
+function watchColorPicker(event) {
+  return (color = event.target.value);
 }
 
-function draw(event){
-    $(this).css('background-color', color);
+function draw(event) {
+  $(this).css("background-color", color);
 }
 
 // Set size picker submit button listener. Call makeGrid on submit.
-sizePickerSubmit.on('click', makeGrid);
+sizePickerSubmit.on("click", makeGrid);
 
 // Set the color picker event listener. Call watchColorPicker on change.
-colorPicker.on('change', watchColorPicker);
+colorPicker.on("change", watchColorPicker);
 
 // Set the table listener
-pixelCanvas.on('click', 'td', draw);
+pixelCanvas.on("click", "td", draw);
 
 // Each cell should have an event listener that sets the background color of the cell to the selected color.
