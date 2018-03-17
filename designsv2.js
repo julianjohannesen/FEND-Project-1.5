953654b4436879326e3315c23ed0bb0d87a70ede
julianjohannesen <julian.johannesen@gmail.com>
2018-03-16 22:30:31 -0400

    removed jquery and replaced with plain js and completely recoded

--- designsv2.js
+++ designsv2.js
@@ -9,8 +9,6 @@ let color = "#000000";
 const pixelCanvas = document.getElementById("pixelCanvas");
 
 // User a nested loop to create a long string storing the table html
-// and then insert it into the DOM
-// Create table
 let tableHtml = "";
 function makeTable() {
   // Get height and width
@@ -21,7 +19,7 @@ function makeTable() {
     tableHtml += "<tr>";
     // for each table td
     for (var j = 0; j < width; j++) {
-      if (j === width.length - 1) {
+      if (j === width - 1) {
         tableHtml += "<td></td></tr>";
       } else {
         tableHtml += "<td></td>";
@@ -31,31 +29,36 @@ function makeTable() {
   return tableHtml;
 }
 
-function insertTable(tableHtml) {
+// insertTable inserts the table html
+function insertTable() {
+  // Set the canvas's innerHTML property to the table html
   pixelCanvas.innerHTML = tableHtml;
 }
 
-// Clicking on the submit button will call tableListener.
-function tableListener(event) {
+// Clicking the size picker form's submit button will call
+// tableListener, which calls makeTable and insertTable
+function tableListener() {
   makeTable();
   insertTable();
 }
 
-// Clicking "okay" in the color picker will call watchColorPicker. watchColorPicker
-// sets a color
+// Clicking "okay" in the color picker will call watchColorPicker, which
+// sets color equal to the color chosen in the color picker
 function watchColorPicker(event) {
-  return (color = event.target.value);
+  color = event.target.value;
 }
 
+// Clicking on the table will change the color of the td where the click
+// took place
 function draw(event) {
   event.target.style.backgroundColor = color;
 }
 
 // Set size picker submit button listener. Call makeTable on submit.
-sizePickerSubmit.addEventListener("click", makeTable, false);
+sizePickerSubmit.addEventListener("click", tableListener, true);
 
 // Set the color picker event listener. Call watchColorPicker on change.
-colorPicker.addEventListener("change", watchColorPicker, false);
+colorPicker.addEventListener("change", watchColorPicker, true);
 
 // Set the table listener
-pixelCanvas.addEventListener("click", draw, false);
+pixelCanvas.addEventListener("click", draw, true);
